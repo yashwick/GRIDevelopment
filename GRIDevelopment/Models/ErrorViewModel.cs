7938e419d08cb9e7c60ea072a3c434724ae68c72
dileepainivos <dileepainivos@gmail.com>
2022-03-02 16:18:42 +0530

    Add project files.

---
+++ GRIDevelopment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GRIDevelopment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
