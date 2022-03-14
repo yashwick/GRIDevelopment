f34b29566af5a883bb9e33c4b9d8e177383ce215
Yashoba Wickramage <yashobaw@gmail.com>
2022-03-14 13:20:12 +0530

    Customer services in BAL and Contract were created

---
+++ BAL/CustomerServices.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL
+{
+    public class CustomerServices:ICustomerServices
+    {
+        private readonly Context _context;
+        private readonly 
+    }
+}
---
+++ BAL/Interfaces/ICustomerServices.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Interfaces
+{
+    public interface ICustomerServices
+    {
+        void AddCustomer(CustomerDTO customer);
+        void UpdatePost(CustomerDTO customer);
+
+        List<CustomerDTO> GetCustomers();
+        CustomerDTO GetCustomerById(int id);
+        void RemovePost(int id);
+    }
+}
