8b4997550b2a8f3625a641abcc8744352a742d3a
Yashoba Wickramage <yashobaw@gmail.com>
2022-03-14 11:26:34 +0530

    connection string was added and configuration was done

--- DAL/DBContext/AppConfiguration.cs
+++ DAL/DBContext/AppConfiguration.cs
@@ -12,6 +12,12 @@ namespace DAL.DBContext
         public AppConfiguration()
         {
             var configBuilder = new ConfigurationBuilder();
+            var path = Path.Combine(Directory.GetCurrentDirectory(),"appsettings.json");
+            configBuilder.AddJsonFile(path,false);
+            var root = configBuilder.Build();
+            var appSetting = root.GetSection("ConnectionString:DefaultConnection");
+            sqlConnectionString = appSetting.Value;
         }
+        public string sqlConnectionString { get; set; }
     }
 }
