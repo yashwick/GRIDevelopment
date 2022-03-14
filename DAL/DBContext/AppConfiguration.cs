298d1eeb4c90ae83af1d3b79371c922a9ebd9a91
Yashoba Wickramage <yashobaw@gmail.com>
2022-03-14 10:37:36 +0530

    Adding Nuget Packages

---
+++ DAL/DBContext/AppConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.DBContext
+{
+    public class AppConfiguration
+    {
+        public AppConfiguration()
+        {
+            var configBuilder = new ConfigurationBuilder();
+        }
+    }
+}
